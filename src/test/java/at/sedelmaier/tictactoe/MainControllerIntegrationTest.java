31c7f783bdcd01a1068994ed42769dd9d704ad98
sedstef <stefan@sedelmaier.at>
2023-01-16 20:36:45 +0100

    add bootstrapt + vue

--- src/test/java/at/sedelmaier/tictactoe/MainControllerIntegrationTest.java
+++ src/test/java/at/sedelmaier/tictactoe/MainControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -25,7 +26,9 @@ public class MainControllerIntegrationTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
-                .andExpect(model().attribute("eventName", is("FIFA 2018")));
+                .andExpect(model().attribute("eventName", is("FIFA 2018")))
+//                .andExpect(xpath("html/head/title").string("Tic-Tac-Toe"))
+                .andDo(print());
     }
 
 }

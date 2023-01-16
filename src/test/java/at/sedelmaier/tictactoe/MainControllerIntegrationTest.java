91f71ceac8f10d6f540b6e11c0e6b6ba46491e66
sedstef <stefan@sedelmaier.at>
2023-01-16 20:04:15 +0100

    add basic intergation test

---
+++ src/test/java/at/sedelmaier/tictactoe/MainControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package at.sedelmaier.tictactoe;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+public class MainControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void givenEmployees_whenGetEmployees_thenReturnJsonArray() throws Exception {
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("eventName", is("FIFA 2018")));
+    }
+
+}
